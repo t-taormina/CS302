a9ea9edc27da25e635eb88a6dacfedcb4abedae6
till-t <tktaormina@gmail.com>
2022-10-18 15:20:28 -0700

    update

---
+++ projects/Project1/project1/all.cpp
@@ -0,0 +1,32 @@
+// Tyler Taormina 
+// taormina@pdx.edu
+// October 2022
+// Program 1
+
+#include "data_structures.h"
+
+// ALL Class
+// ==============================================
+
+// Parameterized constructor that takes an integer argument and creates the table
+// array with a length of the argument passed.
+ALL::ALL(int size){}
+
+// Deconstructor
+ALL::~ALL(void){}
+
+int ALL::insert(Card& to_add){}
+
+// Takes a character array of a name to match and an array where matches will be stored.
+int ALL::retrieve(){}
+
+// Displays all items stored in the hash table.
+int ALL::display_all(void){}
+
+// Displays all items that have a TYPE matching the argument supplied character array.
+int ALL::display_current_hand(){}
+
+// Removes all cards.
+int ALL::remove_all(){}
+
+
--- projects/Project1/project1/cll.cpp
+++ projects/Project1/project1/cll.cpp
@@ -1,1 +1,31 @@
+// Tyler Taormina 
+// taormina@pdx.edu
+// October 2022
+// Program 1
+
+#include "data_structures.h"
+
+// Node Class
+// ==============================================
+Node::Node(){}
+Node::~Node(){}
+Node::Node*& get_next(){}
+void Node::set_next(Node* new_next){}
+
+
+// CLL Class
+// ==============================================
+CLL::CLL(void){}
+CLL::~CLL(void){}
+    
+int CLL::add_card(Card& card_to_add){}
+    
+// remove next card (more like stack than a CLL)
+Card CLL::draw_card(){}
+
+// not useful for this game
+Card CLL::retireve_card(Card& to_remove){}
+int CLL::remove_all(){}
+int CLL::display(void){}
+
 
