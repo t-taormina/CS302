84f686a62141be01975cd7a6b5283856d4bfedc6
till-t <tktaormina@gmail.com>
2022-10-23 23:36:59 -0700

    removed dice.cpp

--- projects/Project1/project1/pawn.cpp
+++ projects/Project1/project1/pawn.cpp
@@ -5,8 +5,14 @@
 
 #include "pawn.h"
 #include <iostream>
+#include <cstdlib>
+#include <time.h>
 using namespace std;
+//Contains pawn and dice class implementation.
 
+
+//Pawn Class 
+//=============================================================
 
 // Default Constructor.
 Pawn::Pawn(): Piece(), position(0){}
@@ -46,5 +52,36 @@ int Pawn::change_position(int arg)
 bool Pawn::is_position(int arg)
 {
   return (position == arg);
+}
+
+// Dice Class
+//=============================================================
+
+// Default Constructor.
+Dice::Dice():  Piece(false, true), value(0) 
+{
+  srand(time(NULL)); // seeds random num gen with current time
+}
+
+// Deconstructor
+Dice::~Dice(){}
+
+// @Dev - Displays member data.
+// Args -> None.
+// Returns -> None.
+void Dice::display() const
+{
+  cout << "Value: " << value << endl;
+}
+
+// @Dev - Generates random number from 1-6.
+// Args -> None.
+// Returns -> Random integer from 1-6 and sets member value to the generated integer.
+int Dice::roll()
+{
+  int result = rand() % 6;
+  result += 1;
+  value = result;
+  return result;
 }
 
