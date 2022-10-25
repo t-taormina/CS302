92086317689898f2d0774e9218215a31c1011032
till-t <tktaormina@gmail.com>
2022-10-24 17:06:00 -0700

    safety commit

--- projects/Project1/src/person.h
+++ projects/Project1/src/person.h
@@ -6,14 +6,17 @@
 #ifndef PERSON_H
 #define PERSON_H
 
-// Contains both person and player headers.
+// Contains both person and player headers. These classes contain the logic that
+// represents a player of the game. 
 //================================================================================
 
 class Person 
 {
     public:
         Person();
+        // Copy constructor for dynamically allocated character arrays.
         Person(const Person& to_copy);
+        // Assignment operator for dynamically allocated character arrays.
         Person& operator=(const Person& arg);
         ~Person();
         void display() const;
@@ -33,14 +36,19 @@ class Player: public Person
     public:
         Player();
         ~Player();
+        // Copy constructor for dynamically allocated character arrays.
         Player(const Player& to_copy);
+        // Assignment operator for dynamically allocated character arrays.
         Player& operator=(const Player& arg);
         void display() const;
+        // Adds argument to existing points data field. If a negative number is provided,
+        // 0 will be added.
         int add_points(int points);
         void display_points();
         bool is_winner(const Player& opponent);
         int add_win();
         int add_loss();
+        // Sets points to 0.
         int reset_points();
  
     protected:
