2ec74fbb99f971070530448cf1ee82e63ff12456
Tyler <tktaormina@gmail.com>
2022-11-14 11:43:21 -0800

    start implementing .cpp

--- projects/Project3/src/concept.cpp
+++ projects/Project3/src/concept.cpp
@@ -5,4 +5,28 @@
 
 #include "concept.h"
 
+Concept::Concept(): name(""){}
+
+Concept::Concept(string & arg_name): name(arg_name){}
+
+Concpt::Concept(const Concept & to_copy)
+{
+  name = to_copy.name;
+}
+
+virtual Concept::~Concept(){}
+
+// Self similar functions.
+virtual void Concept::display() const
+{
+  cout << "Name: " << name << endl;
+}
+
+virtual void Concept::read_in()
+{
+
+}
+virtual void edit();
+virtual bool match(string & to_match);
+
 
