a379619a7afa9013013b7734664062f6de475c1d
Tyler <tktaormina@gmail.com>
2022-12-01 18:49:39 -0800

    Redid derived classes, Home is working. Need Away and Vacation.

--- projects/Project4/src/events.py
+++ projects/Project4/src/events.py
@@ -9,28 +9,22 @@ This file contains the hierarchy used in this project.
 # cSpell:ignore Taormina
 
 from datetime import date
-DEFAULT_DATE = "19910220"
 
 
 class Event:
     """Base class for event hierarchy.
     """
-    def __init__(self, title: str, location: str, date: str):
+    def __init__(self, title: str, location: str, date: date):
         """Initializes Event object."""
         self._title = title
         self._location = location
         self._date = date
-        self.edit_date(date)
 
     def __str__(self):
         """Format _date and returns a formatted string for all data members."""
-        converted_date_format = date(
-                year=int(self._date[0:4]),
-                month=int(self._date[4:6]),
-                day=int(self._date[6:8]))
         return (f'Title: {self._title}\n' +
                 f'Location: {self._location}\n' +
-                f'Date: {converted_date_format}\n')
+                f'Date: {self._date}\n')
 
     def __repr__(self):
         """Returns string repr of Event object."""
@@ -38,130 +32,99 @@ class Event:
                 f"location='{self._location}', " +
                 f"date='{self._date}')")
 
-    def edit(self, choice: int, arg: str):
-        """Edit the
-        """
-        if (0 < choice < 4):
-            # edit title: choice 1
-            if (choice == 1):
-                self.edit_title(arg)
-            # edit location: choice 2
-            if (choice == 2):
-                self.edit_location(arg)
-            # edit date: choice 3
-            if (choice == 3):
-                self.edit_date(arg)
-            return 0
-        else:
-            return 1
-
     def edit_title(self, arg: str):
         """
         """
         self._title = arg
+        return None
 
     def edit_location(self, arg: str):
         """
         """
         self._location = arg
+        return None
 
-    def edit_date(self, arg: str):
-        """Determines if the argument passed is a valid
-        date. If the date is valid, the data member is set.
-        Otherwise, a default date is used to set data
-        member. The default date is the day Python
-        0.9.0 was released.
+    def edit_date(self, arg: date):
         """
-        try:
-            date(year=int(arg[0:4]),
-                 month=int(arg[4:6]),
-                 day=int(arg[6:8]))
-
-            self._date = arg[0:8]
-        except ValueError:
-            print('Invalid date. Format: yyyymmdd\n' +
-                  'Set date value to default: 1991-02-20 PLEASE CHANGE\n')
-            self._date = DEFAULT_DATE
+        """
+        self._date = date
+        return None
 
     def match(self, arg: str):
+        """
+        """
         if arg.lower() == self._title.lower():
             return True
         return False
 
 
-class Vacation(Event):
-    """Vacation class is publicly derived from the Event class.
+class Home(Event):
+    """Home class is publicly derived from the Event class.
     """
-    def __init__(self, end_date: str, title: str, location: str, date: str):
+    def __init__(self, start_time: str, guests, title: str, date: date):
         """Initializes Vacation object."""
-        self._end_date = end_date
-        self.edit_end_date(end_date)
-        self._packing_list = []
-        Event.__init__(self, title, location, date)
+        Event.__init__(self, title, "Home", date)
+        self._start_time = start_time
+        self._guest_list = guests
 
     def __str__(self):
         """Format _end_date and returns a formatted string for all data
         members in base and current class.
         """
-        converted_date_format = date(
-                year=int(self._end_date[0:4]),
-                month=int(self._end_date[4:6]),
-                day=int(self._end_date[6:8]))
         return (
                 Event.__str__(self) +
-                f'End Date: {converted_date_format}\n')
+                f'Start Time: {self.format_time()}\n' +
+                'Guest List: ' +
+                ', '.join([str(item) for item in self._guest_list]) +
+                '\n')
 
     def __repr__(self):
         """Formats the data members to mimic the function call made to
         construct the object.
         """
         return (
-                f"Vacation(end_date='{self._end_date}', " +
+                f"Home(start_time='{self._start_time}', " +
                 f"title='{self._title}', " +
                 f"location='{self._location}', " +
                 f"date='{self._date}')")
 
-    def edit(self, choice: int, arg: str):
-        """Edit the
+    def edit_start_time(self, arg: str):
         """
-        if (0 < choice < 5):
-            # call parent choice 1 - 3
-            if 0 < choice < 4:
-                Event.edit(self, choice, arg)
-            # edit end date: choice 4
-            if (choice == 4):
-                self.edit_end_date(arg)
-            return 0
-        else:
-            return 1
-
-    def edit_end_date(self, arg: str):
-        """Determines if the argument passed is a valid
-        date. If the date is valid, the data member is set.
-        Otherwise, a default date is used to set data
-        member. The default date is the day Python
-        0.9.0 was released.
         """
-        try:
-            date(year=int(arg[0:4]),
-                 month=int(arg[4:6]),
-                 day=int(arg[6:8]))
+        self._start_time = arg
+        return None
 
-            self._end_date = arg[0:8]
-        except ValueError:
-            print('Invalid date. Format: yyyymmdd\n' +
-                  'Set date value to default: 1991-02-20 PLEASE CHANGE\n')
-            self._end_date = DEFAULT_DATE
+    def edit_title(self, arg: str):
+        """
+        """
+        return super().edit_title(arg)
+
+    def edit_location(self, arg: str):
+        """
+        """
+        return super().edit_location(arg)
+
+    def edit_date(self, arg: date):
+        """
+        """
+        return super().edit_date(arg)
 
     def match(self, arg: str):
         return super().match(arg)
 
+    def format_time(self):
+        """
+        """
+        return (f'{self._start_time[0:2]}:' +
+                f'{self._start_time[2:4]}' +
+                f'{self._start_time[4:6]}')
 
-class Meal(Event):
+
+class Away(Event):
     """
     """
     def __init__(self, dress_code: str, start_time: str, title: str,
-                 location: str, date: str):
+                 location: str, date: date):
         Event.__init__(self, title, location, date)
         self._dress_code = dress_code
         self._start_time = start_time
@@ -172,42 +135,19 @@ class Meal(Event):
         """
         return (
                 Event.__str__(self) +
-                f'Dress Code: {self._dress_code}\n' +
-                f'Start Time: {self.format_time()}\n')
+                f'Start Time: {self.format_time()}\n' +
+                f'Dress Code: {self._dress_code}\n')
 
     def __repr__(self):
         """Formats the data members to mimic the function call made to
         construct the object.
         """
         return (
-                f"Meal(dress_code='{self._dress_code}', " +
+                f"Away(dress_code='{self._dress_code}', " +
                 f"start_time='{self._start_time}', " +
                 f"title='{self._title}', " +
                 f"location='{self._location}', " +
                 f"date='{self._date}')")
-
-    def format_time(self):
-        """
-        """
-        return (f'{self._start_time[0:2]}:' +
-                f'{self._start_time[2:4]}' +
-                f'{self._start_time[4:6]}')
-
-    def edit(self, choice: int, arg: str):
-        """Edit the
-        """
-        if (0 < choice < 6):
-            # call parent choice 1 - 3
-            if 0 < choice < 4:
-                Event.edit(self, choice, arg)
-            # edit end date: choice 4
-            if (choice == 4):
-                self.edit_dress_code(arg)
-            if (choice == 5):
-                self.edit_start_time(arg)
-            return 0
-        else:
-            return 1
 
     def edit_start_time(self, arg: str):
         """
@@ -221,38 +161,23 @@ class Meal(Event):
         self._dress_code = arg
         return None
 
+    def edit_title(self, arg: str):
+        """
+        """
+        return super().edit_title(arg)
+
+    def edit_location(self, arg: str):
+        """
+        """
+        return super().edit_location(arg)
+
+    def edit_date(self, arg: date):
+        """
+        """
+        return super().edit_date(arg)
+
     def match(self, arg: str):
         return super().match(arg)
-
-
-class Other(Event):
-    """
-    """
-    def __init__(self, start_time: str, duration: str, title: str,
-                 location: str, date: str):
-        Event.__init__(self, title, location, date)
-        self._duration = duration
-        self._start_time = start_time
-
-    def __str__(self):
-        """Format _end_date and returns a formatted string for all data
-        members in base and current class.
-        """
-        return (
-                Event.__str__(self) +
-                f'Start Time: {self.format_time()}\n' +
-                f'Duration: {self._duration} hours\n')
-
-    def __repr__(self):
-        """Formats the data members to mimic the function call made to
-        construct the object.
-        """
-        return (
-                f"Meal(start_time='{self._start_time}', " +
-                f"duration='{self._duration}', " +
-                f"title='{self._title}', " +
-                f"location='{self._location}', " +
-                f"date='{self._date}')")
 
     def format_time(self):
         """
@@ -261,33 +186,56 @@ class Other(Event):
                 f'{self._start_time[2:4]}' +
                 f'{self._start_time[4:6]}')
 
-    def edit(self, choice: int, arg: str):
-        """Edit the
-        """
-        if (0 < choice < 6):
-            # call parent choice 1 - 3
-            if 0 < choice < 4:
-                Event.edit(self, choice, arg)
-            # edit end date: choice 4
-            if (choice == 4):
-                self.edit_duration(arg)
-            if (choice == 5):
-                self.edit_start_time(arg)
-            return 0
-        else:
-            return 1
 
-    def edit_start_time(self, arg: str):
+class Vacation(Event):
+    """
+    """
+    def __init__(self, end_date: date, title: str,
+                 location: str, date: date):
+        Event.__init__(self, title, location, date)
+        self._end_date = end_date
+        self._packing_list = []
+
+    def __str__(self):
+        """Format _end_date and returns a formatted string for all data
+        members in base and current class.
+        """
+        return (
+                Event.__str__(self) +
+                f'End date: {self._end_date}\n' +
+                'Packing List: ' +
+                ', '.join([str(item) for item in self._packing_list]))
+
+    def __repr__(self):
+        """Formats the data members to mimic the function call made to
+        construct the object.
+        """
+        return (
+                f"Vacation(end_date='{self._end_date}', " +
+                f"title='{self._title}', " +
+                f"location='{self._location}', " +
+                f"date='{self._date}')")
+
+    def edit_end_date(self, arg: date):
         """
         """
-        self._start_time = arg
+        self._end_date = arg
         return None
 
-    def edit_duration(self, arg: str):
+    def edit_title(self, arg: str):
         """
         """
-        self._duration = arg
-        return None
+        return super().edit_title(arg)
+
+    def edit_location(self, arg: str):
+        """
+        """
+        return super().edit_location(arg)
+
+    def edit_date(self, arg: date):
+        """
+        """
+        return super().edit_date(arg)
 
     def match(self, arg: str):
         return super().match(arg)
--- projects/Project4/src/main.py
+++ projects/Project4/src/main.py
@@ -5,13 +5,13 @@ taormina@pdx.edu
 # cSpell:ignore Taormina
 # cSpell:ignore RBnode
 
-from events import Event, Vacation, Meal, Other
+from events import Event, Home, Away, Vacation, date
 from red_black import RBnode
 from aLL import Table
 TABLE_SIZE = 3
-VACATION = 0
-MEAL = 1
-OTHER = 2
+HOME = 0
+AWAY = 1
+VACATION = 2
 
 
 def main():
@@ -27,9 +27,9 @@ def main():
 
 
 def test_table_class():
-    vacation_values = build_vacation_list()
-    meal_values = build_meal_list()
-    other_values = build_other_list()
+    home_values = build_home_list()
+    # away_values = build_away_list()
+    # vacation_values = build_vacation_list()
     """
     list = LinkedList(other_values)
     list.display()
@@ -37,290 +37,31 @@ def test_table_class():
     list.display()
     """
     table = Table(TABLE_SIZE)
-    table.insert(vacation_values, VACATION)
-    table.insert(meal_values, MEAL)
-    table.insert(other_values, OTHER)
+    table.insert(home_values, HOME)
+    # table.insert(meal_values, MEAL)
+    # table.insert(other_values, OTHER)
     # table.display_specific_event("socCer")
+    # table.display_category(HOME)
     print(table)
-    # table.display_category(VACATION)
     # table.remove_event("day TRIP")
     # table.display_category(VACATION)
 
 
-def build_other_list():
-    o1 = Other("1200PM",
-               "2",
-               "Soccer",
-               "53 Ave Park",
-               "20221224")
-    o2 = Other("0500PM",
-               "3",
-               "Charity",
-               "Goodwill",
-               "20221224")
-    o3 = Other("0800AM",
-               "1",
-               "Run",
-               "Nike",
-               "20221224")
-    return [o1, o2, o3]
-
-
-def build_meal_list():
-    m1 = Meal("Casual",
-              "0500PM",
-              "Work Dinner",
-              "Applebees",
-              "20221224")
-    m2 = Meal("Formal",
-              "0600PM",
-              "Lunch",
-              "Mark's Bus",
-              "20221224")
-    m3 = Meal("Semi-Formal",
-              "0700PM",
-              "Cocktails",
-              "Kells",
-              "20221224")
-    m4 = Meal("Casual",
-              "0500PM",
-              "Dinner",
-              "Sushi Hanna",
-              "20221224")
-    m5 = Meal("Business Casual",
-              "0400PM",
-              "Work Dinner",
-              "Mah Now",
-              "20221224")
-    return [m1, m2, m3, m4, m5]
-
-
-def build_vacation_list():
-    v1 = Vacation("20221225",
-                  "day trip",
-                  "oregon",
-                  "20221220")
-
-    v2 = Vacation("20221225",
-                  "Family Vacation",
-                  "California",
-                  "20221220")
-
-    v3 = Vacation("20221225",
-                  "holiday",
-                  "new york",
-                  "20221220")
-
-    v4 = Vacation("20221225",
-                  "beaches",
-                  "costa mesa",
-                  "20221220")
-    return [v1, v2, v3, v4]
-
-
-def test_node_class():
-    meal = Meal("Casual",
-                "0500PM",
-                "Work Dinner",
-                "Applebees",
-                "20221224")
-    vacation = Vacation("20221225",
-                        "Family Vacation",
-                        "California",
-                        "20221220")
-    meal_node = Node(meal)
-    vacation_node = Node(vacation)
-    print(meal_node)
-    print(vacation_node)
-    return None
-
-
-def test_RBnode_class():
-    """
-    """
-    meal = Meal("Casual",
-                "0500PM",
-                "Work Dinner",
-                "Applebees",
-                "20221224")
-    vacation = Vacation("20221225",
-                        "Family Vacation",
-                        "California",
-                        "20221220")
-    meal_node = RBnode(meal)
-    vacation_node = RBnode(vacation)
-    print(meal_node)
-    print(vacation_node)
-    return None
-
-
-def get_time():
-    """
-    """
-    user_input: str = input('Enter time in format: hhmmAM / hhmmPM\n' +
-                            'For example: 0430PM\n->')
-    return user_input
-
-
-def get_duration():
-    user_input: str = input('Enter duration in format: hh\n' +
-                            'For example: 01 for one hour\n->')
-    return user_input
-
-
-def get_date():
-    user_input: str = input('Dates should be entered in format: yyyymmdd\n')
-    return user_input
-
-
-def menu_core():
-    """
-    """
-    return ('Edit Information\n==================\n' +
-            '1) Edit Title \n' +
-            '2) Edit Location \n' +
-            '3) Edit Date\n')
-
-
-def meal_menu():
-    """
-    """
-    return '4) Edit Dress Code\n5) Edit Start Time\n->'
-
-
-def vacation_menu():
-    """
-    """
-    return '4) Edit End Date\n->'
-
-
-def other_menu():
-    """
-    """
-    return '4) Edit Duration\n5) Edit Start Time\n->'
-
-
-def collect_vacation_argument(choice: int):
-    """
-    """
-    if choice == 3 or choice == 4:
-        arg = get_date()
-    else:
-        arg = input('Enter the new data:\n->')
-    return arg
-
-
-def collect_meal_argument(choice: int):
-    """
-    """
-    if choice == 3:
-        arg = get_date()
-    elif choice == 5:
-        arg = get_time()
-    else:
-        arg = input('Enter the new data:\n->')
-    return arg
-
-
-def collect_other_argument(choice: int):
-    """
-    """
-    if choice == 3:
-        arg = get_date()
-    elif choice == 4:
-        arg = get_duration()
-    elif choice == 5:
-        arg = get_time()
-    else:
-        arg = input('Enter the new data:\n->')
-    return arg
-
-
-def edit_meal_menu():
-    """
-    """
-    choice = int(input(menu_core() + meal_menu()))
-    arg = collect_meal_argument(choice)
-    return choice, arg
-
-
-def edit_vacation_menu():
-    """
-    """
-    choice = int(input(menu_core() + vacation_menu()))
-    arg = collect_vacation_argument(choice)
-    return choice, arg
-
-
-def edit_other_menu():
-    """
-    """
-    choice = int(input(menu_core() + other_menu()))
-    arg = collect_other_argument(choice)
-    return choice, arg
-
-
-def test_meal_class():
-    """
-    """
-    meal = Meal("Casual",
-                "0500PM",
-                "Work Dinner",
-                "Applebees",
-                "20221224")
-    print(meal)
-    print(repr(meal))
-    choice, arg = edit_meal_menu()
-    meal.edit(choice, arg)
-    print(meal)
-    print(repr(meal))
-
-
-def test_other_class():
-    """
-    """
-    other = Other("0500PM",
-                  "2",
-                  "Work Dinner",
-                  "Applebees",
-                  "20221224")
-    print(other)
-    print(repr(other))
-    choice, arg = edit_other_menu()
-    other.edit(choice, arg)
-    print(other)
-    print(repr(other))
-
-
-def test_vacation_class():
-    """
-    """
-    vacation = Vacation("20221225",
-                        "Family Vacation",
-                        "California",
-                        "20221220")
-    print(vacation)
-    print(repr(vacation))
-    # make edits
-    choice, arg = edit_vacation_menu()
-    vacation.edit(choice, arg)
-    print(vacation)
-    print(repr(vacation))
-
-    # Copy works for objects
-    copy_vacation = vacation
-    print('\n' + copy_vacation.__str__())
-    return None
-
-
-def test_event_class():
-    """
-    """
-    event = Event("skiing", "mt. hood", "20221220")
-    print(event)
-    print(repr(event))
-    # make edits
-    return None
+def build_home_list():
+    guests = ["Tony", "Alex", "Zach", "Will", "KJ", "Mom", "Frank"]
+    h1 = Home("1200PM",
+              guests,
+              "Family Dinner",
+              date(2022, 12, 23))
+    h2 = Home("0500PM",
+              guests,
+              "BBQ",
+              date(2022, 1, 1))
+    h3 = Home("0800AM",
+              guests,
+              "Breakfast",
+              date(2022, 12, 26))
+    return [h1, h2, h3]
 
 
 if __name__ == '__main__':
