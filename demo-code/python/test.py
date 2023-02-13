aa65596ef7a21dfe47f8f561f22ae8cd3b400d1f
tyler <tktaormina@gmail.com>
2023-02-13 08:45:41 -0800

    git check

--- demo-code/python/test.py
+++ demo-code/python/test.py
@@ -5,8 +5,8 @@ class TestSum(unittest.TestCase):
     def test_sum(self):
         self.assertEqual(sum([1, 2, 3]), 6, "Should be 6")
 
-    def test_sum_tuple(self):
-        self.assertFalse(sum((1, 2, 2)) == 6)
+    def test_sum_tuple_false(self):
+        self.assertEqual(sum((1, 2, 2)) == 6)
 
     def test_remove_last(self):
         name = "tyler"
